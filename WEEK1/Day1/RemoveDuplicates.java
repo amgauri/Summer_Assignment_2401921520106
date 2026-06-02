e1f2c77f9857dd0ba2bed2a640e392121fe023aa
amgauri <gaurikpandey08@gmail.com>
2026-06-02 14:22:36 +0000

    Organized Day 1 files properly

---
+++ WEEK1/Day1/RemoveDuplicates.java
@@ -0,0 +1,31 @@
+class RemoveDuplicates {
+
+    public static int removeDuplicates(int[] nums) {
+
+        int i = 0;
+
+        for (int j = 1; j < nums.length; j++) {
+
+            if (nums[i] != nums[j]) {
+
+                nums[i + 1] = nums[j];
+                i++;
+            }
+        }
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 1, 2, 2, 3, 4};
+
+        int k = removeDuplicates(nums);
+
+        System.out.println("Unique elements count: " + k);
+
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
