c02827ed0f88d245fc91f0a9b2eadae567cda297
amgauri <gauripandey@Gauris-MacBook-Air.local>
2026-06-13 23:56:18 +0530

    optimised the solution

--- WEEK2/Day3/RepeatedSubstringPattern.java
+++ WEEK2/Day3/RepeatedSubstringPattern.java
@@ -1,39 +1,18 @@
 package WEEK2.Day3;
 
-public class RepeatedSubstringPattern  {
+public class RepeatedSubstringPattern {
 
     public boolean repeatedSubstringPattern(String s) {
-
-        int totalLength = s.length();
-
-        for (int pieceLength = 1; pieceLength <= totalLength / 2; pieceLength++) {
-
-            if (totalLength % pieceLength != 0) {
-                continue;
-            }
-
-            String candidate = s.substring(0, pieceLength);
-
-            StringBuilder formedString = new StringBuilder();
-
-            int repeatCount = totalLength / pieceLength;
-
-            for (int turn = 0; turn < repeatCount; turn++) {
-                formedString.append(candidate);
-            }
-
-            if (formedString.toString().equals(s)) {
-                return true;
-            }
-        }
-
-        return false;
+        String doubled = s + s;
+        String trimmed = doubled.substring(1, doubled.length() - 1);
+        return trimmed.contains(s);
     }
 
     public static void main(String[] args) {
         RepeatedSubstringPattern solver = new RepeatedSubstringPattern();
 
-        System.out.println(solver.repeatedSubstringPattern("abab"));
+        System.out.println(solver.repeatedSubstringPattern("abab"));          // true
+        System.out.println(solver.repeatedSubstringPattern("aba"));           // false
+        System.out.println(solver.repeatedSubstringPattern("abcabcabcabc"));  // true
     }
 }
-
