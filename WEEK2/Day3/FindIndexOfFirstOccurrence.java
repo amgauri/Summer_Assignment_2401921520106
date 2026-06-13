9a1deed3f453ccd8df024a05e6fb18f4ae86ea25
amgauri <gauripandey@Gauris-MacBook-Air.local>
2026-06-14 00:01:37 +0530

    optimised the code

--- WEEK2/Day3/FindIndexOfFirstOccurrence.java
+++ WEEK2/Day3/FindIndexOfFirstOccurrence.java
@@ -1,3 +1,4 @@
+
 package WEEK2.Day3;
 
 public class FindIndexOfFirstOccurrence {
@@ -8,27 +9,66 @@ public class FindIndexOfFirstOccurrence {
             return 0;
         }
 
-        for (int startPos = 0; startPos <= haystack.length() - needle.length(); startPos++) {
+        int[] lps = buildLPS(needle);
 
-            int matchIndex = 0;
+        int i = 0; // haystack pointer
+        int j = 0; // needle pointer
 
-            while (matchIndex < needle.length()
-                    && haystack.charAt(startPos + matchIndex) == needle.charAt(matchIndex)) {
-                matchIndex++;
-            }
+        while (i < haystack.length()) {
 
-            if (matchIndex == needle.length()) {
-                return startPos;
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+
+                if (j == needle.length()) {
+                    return i - j;
+                }
+            } else {
+
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
             }
         }
 
         return -1;
     }
 
-    public static void main(String[] args) {
-        FindIndexOfFirstOccurrence solver = new FindIndexOfFirstOccurrence();
+    private int[] buildLPS(String pattern) {
 
-        System.out.println(solver.strStr("sadbutsad", "sad"));
+        int[] lps = new int[pattern.length()];
+
+        int len = 0;
+        int i = 1;
+
+        while (i < pattern.length()) {
+
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        return lps;
+    }
+
+    public static void main(String[] args) {
+
+        FindIndexOfFirstOccurrence solver =
+                new FindIndexOfFirstOccurrence();
+
+        System.out.println(
+                solver.strStr("sadbutsad", "sad"));
     }
 }
-
